89912932d1f8f4db38889ce9c646234e87503e8e
Jeff Foster <jeff.foster@red-gate.com>
2022-07-15 17:15:46 +0100

    move to local func

--- Program.cs
+++ Program.cs
@@ -3,31 +3,36 @@
 // Vec3
 using System.Numerics;
 
-const int width = 256;
-const int height = 256;
+static void WriteTestImage() {
 
-var image = new float[width][][];
-for (int i=0; i<width; i++) {
-    image[i] = new float[height][];
-    for (int j=0; j<height; j++) {
-        image[i][j] = new float[3];
+    const int width = 256;
+    const int height = 256;
+
+    var image = new float[width][][];
+    for (int i=0; i<width; i++) {
+        image[i] = new float[height][];
+        for (int j=0; j<height; j++) {
+            image[i][j] = new float[3];
+        }
     }
+
+    for (int j = height-1; j >= 0; --j) {
+        for (int i = 0; i < width; ++i) {
+            var r = (double)i / (width-1);
+            var g = (double)j / (height-1);
+            var b = 0.25;
+
+            int ir = (int)(255.999 * r);
+            int ig = (int)(255.999 * g);
+            int ib = (int)(255.999 * b);
+
+            image[i][j][0] = ir;
+            image[i][j][1] = ig;
+            image[i][j][2] = ib;
+        }
+    }
+
+    PPM.WritePPM(image, "out.ppm");
 }
 
-for (int j = height-1; j >= 0; --j) {
-    for (int i = 0; i < width; ++i) {
-        var r = (double)i / (width-1);
-        var g = (double)j / (height-1);
-        var b = 0.25;
-
-        int ir = (int)(255.999 * r);
-        int ig = (int)(255.999 * g);
-        int ib = (int)(255.999 * b);
-
-        image[i][j][0] = ir;
-        image[i][j][1] = ig;
-        image[i][j][2] = ib;
-    }
-}
-
-PPM.WritePPM(image, "out.ppm");
+WriteTestImage();
